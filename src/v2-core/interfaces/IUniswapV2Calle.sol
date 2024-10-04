4af57ffba95d315887d233cb78b8c93c4cbb5ef8
Maikel Ordaz <maikelordaz@gmail.com>
2024-10-03 21:53:46 -0400

    interfaces in 0.8.27

---
+++ src/v2-core/interfaces/IUniswapV2Calle.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.27;
+
+interface IUniswapV2Callee {
+    function uniswapV2Call(
+        address sender,
+        uint amount0,
+        uint amount1,
+        bytes calldata data
+    ) external;
+}
