309e62842026a3dd26d49421b1bfd2013f82c5d6
Maikel Ordaz <maikelordaz@gmail.com>
2024-10-03 22:53:17 -0400

    ERC20

---
+++ src/v2-core/test/ERC20.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.27;
+
+import {UniswapV2ERC20} from "src/v2-core/UniswapV2ERC20.sol";
+
+contract ERC20 is UniswapV2ERC20 {
+    constructor(uint _totalSupply) public {
+        _mint(msg.sender, _totalSupply);
+    }
+}
