0dc80e744b6375385418e1be11a08e1d70bbe343
Maikel Ordaz <maikelordaz@gmail.com>
2024-10-21 17:24:57 -0400

    AddLiquid puzzle

---
+++ src/puzzles/AddLiquid.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./interfaces/IUniswapV2Pair.sol";
+
+contract AddLiquid {
+    /**
+     *  ADD LIQUIDITY WITHOUT ROUTER EXERCISE
+     *
+     *  The contract has an initial balance of 1000 USDC and 1 WETH.
+     *  Mint a position (deposit liquidity) in the pool USDC/WETH to msg.sender.
+     *  The challenge is to provide the same ratio as the pool then call the mint function in the pool contract.
+     *
+     */
+    function addLiquidity(
+        address usdc,
+        address weth,
+        address pool,
+        uint256 usdcReserve,
+        uint256 wethReserve
+    ) public {
+        IUniswapV2Pair pair = IUniswapV2Pair(pool);
+
+        // your code start here
+
+        // see available functions here: https://github.com/Uniswap/v2-core/blob/master/contracts/interfaces/IUniswapV2Pair.sol
+
+        // pair.getReserves();
+        // pair.mint(...);
+
+        IUniswapV2Pair _usdc = IUniswapV2Pair(usdc);
+        IUniswapV2Pair _weth = IUniswapV2Pair(weth);
+
+        _usdc.transfer(pool, 1000 * 10 ** 6);
+        _weth.transfer(pool, 1 ether);
+        pair.mint(msg.sender);
+    }
+}
