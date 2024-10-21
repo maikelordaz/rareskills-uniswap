9f6f503d611de95c97cb13299752ca2836783645
Maikel Ordaz <maikelordaz@gmail.com>
2024-10-21 17:28:00 -0400

    SimpleSwap puzzle

---
+++ src/puzzles/SimpleSwap.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IERC20.sol";
+
+contract SimpleSwap {
+    /**
+     *  PERFORM A SIMPLE SWAP WITHOUT ROUTER EXERCISE
+     *
+     *  The contract has an initial balance of 1 WETH.
+     *  The challenge is to swap any amount of WETH for USDC token using the `swap` function
+     *  from USDC/WETH pool.
+     *
+     */
+    function performSwap(address pool, address weth, address usdc) public {
+        /**
+         *     swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data);
+         *
+         *     amount0Out: the amount of USDC to receive from swap.
+         *     amount1Out: the amount of WETH to receive from swap.
+         *     to: recipient address to receive the USDC tokens.
+         *     data: leave it empty.
+         */
+
+        // your code start here
+        IERC20(weth).transfer(pool, 1 ether);
+        IUniswapV2Pair(pool).swap(1_000 * 1e6, 0, address(this), new bytes(0));
+    }
+}
